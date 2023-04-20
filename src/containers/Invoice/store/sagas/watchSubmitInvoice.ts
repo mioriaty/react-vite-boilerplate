165d6bfaa6f75c6474abda5a6121e92fff396de4
mioriaty <mioriaty@gmail.com>
2023-04-20 16:40:42 +0700

    them notify submit form

--- src/containers/Invoice/store/sagas/watchSubmitInvoice.ts
+++ src/containers/Invoice/store/sagas/watchSubmitInvoice.ts
@@ -1,5 +1,6 @@
 import { submitInvoiceFailed, submitInvoicePending, submitInvoiceSucceed } from '@app/containers/Invoice/store/actions';
 import { invoiceService } from '@app/services/Invoice';
+import { notification } from 'antd';
 import { put, retry, takeLatest } from 'redux-saga/effects';
 
 function* handleSubmit({ payload: { invoice, type } }: ReturnType<typeof submitInvoicePending>) {
@@ -7,6 +8,7 @@ function* handleSubmit({ payload: { invoice, type } }: ReturnType<typeof submitI
     yield retry(3, 1000, invoiceService.submitInvoice, type, invoice);
     yield put(submitInvoiceSucceed(type));
   } catch (error) {
+    notification.error({ message: (error as Error).message });
     yield put(submitInvoiceFailed(type));
   }
 }
