dee291c4c98a6bd3e4aa8add5733736fa263e60e
mioriaty <mioriaty@gmail.com>
2023-06-03 09:27:33 +0700

    add demo CacheStorage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -12,11 +12,49 @@ import {
   DemoUseTransition,
   DemoWebWorker,
 } from '@app/containers/DemoFeatures';
-import { FC } from 'react';
+import { http } from '@app/httpHandler';
+import { FC, useEffect, useState } from 'react';
 
 export const HomePage: FC = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const cacheStorage = window.caches;
+      const cache = await cacheStorage.open('my-cache');
+      const url = 'https://randomuser.me/api/';
+
+      // check response đã được lưu trong CacheStorage hay chưa
+      const cachedResponse = await cache.match(url);
+      if (cachedResponse) {
+        const cachedData = await cachedResponse.json();
+        setData(cachedData);
+      } else {
+        const response = await http.request({
+          url,
+        });
+        setData(response.data);
+        await cache.put(url, new Response(JSON.stringify(response.data)));
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log('error', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div css={{ padding: '10px' }}>
+      <div>{data && !loading ? <pre>{JSON.stringify(data)}</pre> : <p>Loading...</p>}</div>
+      <button onClick={fetchData}>Reload</button>
+      <hr />
+      <hr />
       <Highlight highlightCss={{ backgroundColor: '#FEEBC8', padding: '2px' }} query={['spotlight', 'you']}>
         With the Highlight component, you can spotlight words.
       </Highlight>
