1209827da640da32e90138e42bff4722ffc397fc
mioriaty <mioriaty@gmail.com>
2023-12-27 15:31:52 +0700

    fix demo

--- src/containers/DemoFeatures/Todo/DemoTodo.tsx
+++ src/containers/DemoFeatures/Todo/DemoTodo.tsx
@@ -2,7 +2,7 @@ import { AsyncComponent } from '@app/components/AsyncComponent';
 import { Button } from '@app/components/Button';
 import { useAppDispatch, useAppSelector } from '@app/hooks/useAppRedux';
 
-import { getTodosWithThunk, todoSelector } from './store';
+import { todoActions, todoSelector } from './store';
 
 export const DemoTodo = () => {
   const { todo, getStatus } = useAppSelector(todoSelector);
@@ -13,7 +13,7 @@ export const DemoTodo = () => {
       <h2>Demo todo</h2>
       <Button
         onClick={async () => {
-          dispatch(getTodosWithThunk('hehe'));
+          dispatch(todoActions.getTodos({ search: 'test' }));
         }}
         css={{ marginRight: '8px' }}
       >
